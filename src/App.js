5ca6797443619cdd80b5e9957be873ab3af07ee4
Travis Reed <traviswreed2@gmail.com>
2020-12-25 23:09:37 -0600

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Directory from './components/DirectoryComponent';
+import { CAMPSITES } from './shared/campsites';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          campsites: CAMPSITES
+      };
+  }
     render() {
         return (
             <div className="App">
@@ -13,7 +20,7 @@ class App extends Component {
                     
                 </div>
                 </Navbar>
-                <Directory />
+                <Directory campsites={this.state.campsites} />
             </div>
         );
     }

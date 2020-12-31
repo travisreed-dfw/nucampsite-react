523366b4c9d429d17caa67f364690c7d2acaedae
Travis Reed <traviswreed2@gmail.com>
2020-12-31 15:09:08 -0600

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
-import Directory from './components/DirectoryComponent';
-import CampsiteInfo from './components/CampsiteInfoComponent';
-import { CAMPSITES } from './shared/campsites';
+import React, { Component } from "react";
+import Main from "./components/MainComponent";
+import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          campsites: CAMPSITES
-      };
-  }
     render() {
         return (
-            <div className="App">
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                    
-                </div>
-                </Navbar>
-                <Directory campsites={this.state.campsites} />
+            <div className='App'>
+                <Main />
             </div>
         );
     }
--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,12 +1,5 @@
 import React, { Component } from "react";
-import {
-    Card,
-    CardImg,
-    CardImgOverlay,
-    CardText,
-    CardBody,
-    CardTitle,
-} from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 class CampsiteInfo extends Component {
     constructor(props) {
@@ -58,9 +51,11 @@ class CampsiteInfo extends Component {
     render() {
         if (this.props.campsite) {
             return (
-                <div className='row'>
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className='container'>
+                    <div className='row'>
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
         } else {
